bc8af8fff3823333b810252fc3edc2655f2bdc3e
joyqi <joyqi@segmentfault.com>
2022-03-15 15:40:12 +0800

    Init project

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # tiny-mixpanel
-A minium mixpanel library written in TypeScript
+
+## What's this?
+
+It's a minium mixpanel library written in TypeScript.
+
+**Why not [mixpanel-browser](https://www.npmjs.com/package/mixpanel-browser)?**
+
+Of course, you can use this official library. But it will make over 20kb extra sizes to a bundled js file. Considering the tracking system is widely used on every page, it will significantly increase the cost of your traffic.
+
+## Install
+
+```
+npm i -S tiny-mixpanel
+```
+
+For TypeScript you should also
+
+```
+npm i -D @types/mixpanel
+```
+
+## Usage
+
+JavaScript
+
+```
+const { loadMixpanel } = require('tiny-mixpanel');
+
+loadMixpanel();
+
+mixpanel.init('xxxx-xxxxxxxxxxxxxxx');
+mixpanel.track('event name');
+```
+
+TypeScript
+
+```
+import { loadMixpanel } from 'tiny-mixpanel';
+
+loadMixpanel();
+
+mixpanel.init('xxxx-xxxxxxxxxxxxxxx');
+mixpanel.track('event name');
+```
---
+++ src/module.ts
@@ -0,0 +1,84 @@
+// load mixpanel
+export function loadMixpanel() {
+    const trackUrl = '//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js';
+    const script = document.createElement('script');
+    const w = window as any;
+
+    const methods = ('disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group'
+        + ' register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking'
+        + ' has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders people.set people.set_once people.unset people.increment'
+        + ' people.append people.union people.track_charge people.clear_charges people.delete_user people.remove').split(' ');
+
+    const mp: any = [];
+
+    mp._i = [];
+    mp.__SV = 1.2;
+
+    mp.init = (token: string, config?: Mixpanel.Config, libraryName?: string): any => {
+        function bind(obj: any, method: string) {
+            if (method.indexOf('.') > 0) {
+                const [key, m] = method.split('.');
+                obj = obj[key];
+                method = m;
+            }
+
+            obj[method] = (...args: any[]) => {
+                obj.push([method].concat(args));
+            }
+        }
+
+        let current = mp as any;
+        current.people = current.people || [];
+
+        if (libraryName) {
+            current = (mp as any)[libraryName] = [];
+        } else {
+            libraryName = 'mixpanel';
+        }
+
+        current.toString = (root = false): string => {
+            let str = 'mixpanel';
+
+            if (libraryName !== 'mixpanel') {
+                str += '.' + libraryName;
+            }
+
+            if (!root) {
+                str += ' (stub)';
+            }
+
+            return str;
+        };
+
+        current.people.toString = () => {
+            return current.toString(true) + '.people (stub)';
+        };
+
+        methods.forEach(method => bind(current, method));
+        const groupMethods = 'set set_once union unset remove delete'.split(' ');
+
+        current.get_group = (...args: any[]) => {
+            const group: Record<string, any> = {};
+            const call = ['get_group'].concat(args);
+
+            function bindGroup(method: string) {
+                group[method] = (...args: any[]) => {
+                    const params = [method].concat(args);
+                    current.push(call, params);
+                };
+            }
+
+            groupMethods.forEach(method => bindGroup(method));
+
+            return group;
+        };
+
+        mp._i.push([token, config, libraryName]);
+    };
+
+    w.mixpanel = mp;
+
+    script.async = true;
+    script.src = trackUrl;
+    document.head.append(script);
+}
